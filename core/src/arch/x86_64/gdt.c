823c6ab259c2a0d105b9fcdb106a090105c18bac
WuX <wux@thenest.dev>
2025-01-28 12:20:16 +0200

    fix(x86_64): set the accessed bit for gdt entries

--- core/src/arch/x86_64/gdt.c
+++ core/src/arch/x86_64/gdt.c
@@ -5,30 +5,30 @@ x86_64_gdt_entry_t g_x86_64_gdt[] = {
     {},
     { // code 16
         .low_limit = 0xFFFF,
-        .access_byte = 0b10011010,
+        .access_byte = 0b10011011,
         .flags_upper_limit = 0b00001111
     },
     { // data 16
         .low_limit = 0xFFFF,
-        .access_byte = 0b10010010,
+        .access_byte = 0b10010011,
         .flags_upper_limit = 0b00001111
     },
     { // code 32
         .low_limit = 0xFFFF,
-        .access_byte = 0b10011010,
+        .access_byte = 0b10011011,
         .flags_upper_limit = 0b11001111
     },
     { // data 32
         .low_limit = 0xFFFF,
-        .access_byte = 0b10010010,
+        .access_byte = 0b10010011,
         .flags_upper_limit = 0b11001111
     },
     { // code 64
-        .access_byte = 0b10011010,
+        .access_byte = 0b10011011,
         .flags_upper_limit = 0b00100000
     },
     { // data 64
-        .access_byte = 0b10010010
+        .access_byte = 0b10010011
     }
 };
 // clang-format on
